24e8f64210a60b39ca9e1652669680044ee9fe2d
Shourya-Dark <77832222+Shourya-Dark@users.noreply.github.com>
2021-05-29 18:04:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var canvas, backgroundimage;
+var gameState = 0;
+var playerCount;
+var database;
+var form, game, player;
+
+ function setup(){
+     
+database = firebase.database();
+ 
+ canvas = createCanvas(400,400);
+ 
+ game = new Game();
+ game.getState();
+ game.start();
+
+} 
+   
+function draw(){ 
+    
+background("white");
+
+drawSprites(); 
+
+}
